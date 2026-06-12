12cdedabf7e20795a773d564f9638dd03dba3023
RAHUL  DAGDI <113167823+RahulDagdi@users.noreply.github.com>
2026-06-12 12:47:42 +0530

    Update resume link in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -111,7 +111,7 @@ transition={{duration:1 }}
        " >
        
 
- <a href='https://drive.google.com/file/d/1tF-uDBi-1wrlFxuH0ysSFPALfehG4FRb/view?usp=drivesdk'  target='_blank'>RESUME</a>
+ <a href='https://drive.google.com/file/d/1yuTA0-cBeGEASQjxu6v7GjYqA7Q7vphT/view?usp=drivesdk'  target='_blank'>RESUME</a>
        </motion.button>
 
 
